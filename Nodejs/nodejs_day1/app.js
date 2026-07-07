d1ba54c226679691838f924c50a6910d7f6c74b4
Khushboo164 <khushboopirwani@gmail.com>
2026-07-07 10:49:03 +0530

    basics of node.js , global objects in node.js , modules in node.js

---
+++ Nodejs/nodejs_day1/app.js
@@ -0,0 +1,7 @@
+//importing module from math.js
+
+const {add,sub,mul,div} = require("./math")
+console.log(add(10,20));
+console.log(sub(20,10));
+console.log(mul(4,6));
+console.log(div(10,2));
---
+++ Nodejs/nodejs_day1/globalObjects.js
@@ -0,0 +1,9 @@
+//global objects
+
+console.error("Error"); //prints errors
+console.warn("Warning"); //prints warnings
+console.log(process.version); //gives current node.js version
+console.log(process.platform); //gives current platform name
+
+console.log(__dirname); //givers current directory path
+console.log(__filename); //gives current file path
